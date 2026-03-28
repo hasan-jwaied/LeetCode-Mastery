41f6000c736f40a732d01ba42d54667afad59288
Hasan Jwaied <jweedhasan@gmail.com>
2026-03-28 11:12:32 +0300

    Greedy-container with most water

---
+++ Greedy/11-M-Container-With-Most-Water/container_with_most_watter.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int start = 0;
+        int end = height.size() - 1;
+        int result = 0;
+
+        int temp = 0;
+
+        while (start < end) {
+            temp = (end-start) * min(height[start], height[end]);
+            if (temp > result)
+                result = temp;
+            
+            if (height[start] < height[end])
+                start++;
+            else
+                end--;
+        }
+        return result;
+    }
+};
