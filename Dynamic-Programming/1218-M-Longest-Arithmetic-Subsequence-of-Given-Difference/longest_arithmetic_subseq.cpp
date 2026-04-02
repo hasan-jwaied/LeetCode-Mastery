cf1bb5a37f394e8ffda5380c59ab08db334e3736
Hasan Jwaied <jweedhasan@gmail.com>
2026-04-02 12:32:53 +0300

    longest arithmetic subseq.

--- Dynamic-Programming/1218-M-Longest-Arithmetic-Subsequence-of-Given-Difference/longest_arithmetic_subseq.cpp
+++ Dynamic-Programming/1218-M-Longest-Arithmetic-Subsequence-of-Given-Difference/longest_arithmetic_subseq.cpp
@@ -4,6 +4,15 @@ public:
         int n = arr.size();
         if (n <= 1) return n;
 
-        
+        unordered_map<int, int> seen;
+
+        int result = 1;
+        for (auto num: arr) {
+            seen[num] = seen[num - difference] + 1;
+            if (result < seen[num]) {
+                result = seen[num];
+            }
+        }
+        return result;
     }
 };
