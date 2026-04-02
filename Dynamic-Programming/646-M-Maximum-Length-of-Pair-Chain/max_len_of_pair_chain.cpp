4a88b8070bea06a72e5138fa9fe2d4a3363d7321
Hasan Jwaied <jweedhasan@gmail.com>
2026-04-02 09:18:06 +0300

    maximum len of pair chain

--- Dynamic-Programming/646-M-Maximum-Length-of-Pair-Chain/max_len_of_pair_chain.cpp
+++ Dynamic-Programming/646-M-Maximum-Length-of-Pair-Chain/max_len_of_pair_chain.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
-        
+        int n = pairs.size();
+        if (n <= 1) return n;
+
+        sort(pairs.begin(), pairs.end());
+        vector<int> OPT(n, 1);
+
+        int result = 0;
+        for (int i=n-2; i>=0; i--) {
+            for (int j=1+i; j<n; j++) {
+                if (pairs[i][1] < pairs[j][0]) {
+                    OPT[i] = max(OPT[i], OPT[j] + 1);
+                    if (result < OPT[i])
+                        result = OPT[i];
+                }
+            }
+        }
+        return result;
     }
 };
