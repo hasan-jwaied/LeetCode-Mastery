1e2043f7ea6d7f1d68c432b6c69a906ea8aec0d4
Hasan Jwaied <jweedhasan@gmail.com>
2026-04-04 10:51:40 +0300

    Find the longest valid obstacle course at each position

---
+++ Dynamic-Programming/1964-H-Find-the-Longest-Valid-Obstacle-Course-at-Each-Position/find_longest_valid_obstacle_course.py
@@ -0,0 +1,17 @@
+from bisect import bisect_right
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, obstacles: list[int]) -> list[int]:
+        ans: list[int] = []
+        tails: list[int]= []
+        
+        
+        for obs in obstacles:
+            idx = bisect_right(tails, obs)
+            
+            if idx == len(tails):
+                tails.append(obs)
+            else:
+                tails[idx] = obs
+            
+            ans.append(idx + 1)
+        return ans
