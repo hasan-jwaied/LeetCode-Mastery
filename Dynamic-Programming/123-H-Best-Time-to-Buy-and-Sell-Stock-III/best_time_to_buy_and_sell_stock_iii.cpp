2ebd9fa1da6c6d79ea9fbce6fe573e8c4f9fe340
Hasan Jwaied <jweedhasan@gmail.com>
2026-04-07 18:50:36 +0300

    best time to by and sell stock iii

--- Dynamic-Programming/123-H-Best-Time-to-Buy-and-Sell-Stock-III/best_time_to_buy_and_sell_stock_iii.cpp
+++ Dynamic-Programming/123-H-Best-Time-to-Buy-and-Sell-Stock-III/best_time_to_buy_and_sell_stock_iii.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int n = prices.size();
+        if (n<=1) return 0;
+
+        int state1 = -prices[0];
+        int state2 = INT_MIN;
+        int state3 = INT_MIN;
+        int state4 = INT_MIN;
+
+        for (int i=0; i<n; i++) {
+            state1 = max(state1, -prices[i]);
+            state2 = max(state2, state1+prices[i]);
+            state3 = max(state3, state2-prices[i]);
+            state4 = max(state4, state3+prices[i]);
+        }
+        return max(state4, 0);
     }
 };
