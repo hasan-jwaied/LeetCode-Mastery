79933ecbc2e336066e0ef90de59c92a26870d661
Hasan Jwaied <jweedhasan@gmail.com>
2026-04-09 13:44:57 +0300

    Unique BST using catalan's num

--- Dynamic-Programming/96-M-Unique-Binary-Search-Trees/unique_bst.cpp
+++ Dynamic-Programming/96-M-Unique-Binary-Search-Trees/unique_bst.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    int factorial (int n) {
-        if (n <= 1)
-            return 1;
-        return factorial(n-1)*n;
-    }
-
     int numTrees(int n) {
-        if (n <= 1)
-            return 1;
-        return factorial(2*n)/(factorial(n)*factorial(n+1));    
+        long long ans = 1; 
+        for (int i = 1; i <= n; ++i)
+            ans = ans * 2 * (2 * i - 1) / (i + 1);
+        return ans;
     }
 };
