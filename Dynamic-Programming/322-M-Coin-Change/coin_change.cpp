fb33cce14f32298c47bd257158bb6b1bb514bd7b
Hasan Jwaied <jweedhasan@gmail.com>
2026-04-13 01:05:27 +0300

    coin chagne

--- Dynamic-Programming/322-M-Coin-Change/coin_change.cpp
+++ Dynamic-Programming/322-M-Coin-Change/coin_change.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> OPT(amount + 1, INT_MAX);
+        vector<int> OPT(amount + 1, amount+1);
         OPT[0] = 0;
         
         for (int i=1; i<amount+1; i++) {
-            for (int j=0; i<n; i++) {
-                if (coins[j] <= i) {
-                    OPT[i] = min(OPT[i], OPT[i-coins[j]]);
-                }
+            for (const auto& coin : coins) {
+                if (coin <= i)
+                    OPT[i] = min(OPT[i], OPT[i-coin] + 1);
             }
         }
-        if (OPT[amount] == INT_MAX) {
+        if (OPT[amount] == amount+1) {
             return -1;
         }
         return OPT[amount];
