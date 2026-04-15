9e8e1de29ea2a14f451d604c4599d3da645c9b95
Hasan Jwaied <jweedhasan@gmail.com>
2026-04-15 20:15:30 +0300

    Coin Change II

---
+++ Dynamic-Programming/518-M-Coin-Change-II/coin_change_ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int m = amount;
+        int n = coins.size();
+
+        vector<vector<int>> OPT (m+1, vector<int>(n+1, 0));
+        for (int j=0; j<n+1; j++) {
+            OPT[0][j] = 1;
+        }
+
+        for (int i=1; i<m+1; i++) {
+            for (int j=1; j<n+1; j++) {
+                int cj = coins[j];
+                if (cj > i) {
+                    OPT[i][j] = OPT[i][j-1];
+                } else {
+                    OPT[i][j] = OPT[i][j-1] + OPT[i-cj][j];
+                }
+            }
+        }
+        return OPT[m][n];
+    }
+};
